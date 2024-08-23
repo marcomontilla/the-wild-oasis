29ade31461ea2604f57e20c2ff8458bd5b7fc830
Marco Montilla <marco.montilla@telrite.com>
2024-08-23 12:15:37 -0400

    what the hell happened here

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+styled.h1`
+  font-size: 30px;
+`
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+    </>
+  );
+}
+
+export default App;
