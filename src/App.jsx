843ac3f8eda3ae1d7fa06d0c5a2f2c3d3c6bc6f2
Marco Montilla <marco.montilla@telrite.com>
2024-08-23 14:07:04 -0400

    temp commit - vid 330

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles.js";
 
-styled.h1`
+const H1 = styled.h1`
   font-size: 30px;
-`
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-600);
+  color: var(--color-brand-50);
+  box-shadow: var(--shadow-sm);
+  cursor: pointer;
+`;
+
+const Input = styled.input`
+  border: 1px solid var(--color-grey-300);
+  background-color: var(--color-grey-0);
+  border-radius: var(--border-radius-sm);
+  padding: 0.8rem 1.2rem;
+  box-shadow: var(--shadow-sm);
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
 
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <GlobalStyles />
+      <H1>Hello world</H1>
     </>
   );
 }
