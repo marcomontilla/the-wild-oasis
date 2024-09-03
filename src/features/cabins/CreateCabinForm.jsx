370f1d9c5a0c64702267b8c1cf6d8df204bd12e7
Marco Montilla <marco.montilla@telrite.com>
2024-09-03 12:36:30 -0400

    355. Uploading Images to Supabase

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -1,5 +1,3 @@
-import styled from "styled-components";
-
 import Input from "../../ui/Input";
 import Form from "../../ui/Form";
 import Button from "../../ui/Button";
@@ -10,42 +8,6 @@ import { useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createCabin } from "../../services/apiCabins";
 import toast from "react-hot-toast";
-
-const FormRowAlt = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
-
-  padding: 1.2rem 0;
-
-  &:first-child {
-    padding-top: 0;
-  }
-
-  &:last-child {
-    padding-bottom: 0;
-  }
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
-`;
-
-const Label = styled.label`
-  font-weight: 500;
-`;
-
-const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
-`;
 
 function CreateCabinForm() {
   const { register, handleSubmit, reset, getValues, formState } = useForm();
@@ -64,7 +26,7 @@ function CreateCabinForm() {
   });
 
   function onSubmit(data) {
-    mutate(data);
+    mutate({ ...data, image: data.image[0] });
   }
 
   function onError(errors) {
@@ -125,7 +87,10 @@ function CreateCabinForm() {
         />
       </FormRow>
 
-      <FormRow label="Description for website" error={errors?.description?.message}>
+      <FormRow
+        label="Description for website"
+        error={errors?.description?.message}
+      >
         <Textarea
           type="number"
           id="description"
@@ -136,18 +101,23 @@ function CreateCabinForm() {
         />
       </FormRow>
 
-      <FormRowAlt>
-        <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" />
-      </FormRowAlt>
+      <FormRow label="Cabin photo">
+        <FileInput
+          id="image"
+          accept="image/*"
+          {...register("image", {
+            required: "This field is required",
+          })}
+        />
+      </FormRow>
 
-      <FormRowAlt>
+      <FormRow>
         {/* type is an HTML attribute! */}
         <Button variation="secondary" type="reset">
           Cancel
         </Button>
         <Button disabled={isPending}>Add cabin</Button>
-      </FormRowAlt>
+      </FormRow>
     </Form>
   );
 }
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6ZWJxdWdubG94YXRmZ2Rxd25lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ3Nzc0NzYsImV4cCI6MjA0MDM1MzQ3Nn0.8XzKmAkJCcfqzrwNCaL9nXXNvCPuUsxNxnj7H23heFM";
-const url = "https://gzebqugnloxatfgdqwne.supabase.co";
+export const supabaseUrl = "https://gzebqugnloxatfgdqwne.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6ZWJxdWdubG94YXRmZ2Rxd25lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ3Nzc0NzYsImV4cCI6MjA0MDM1MzQ3Nn0.8XzKmAkJCcfqzrwNCaL9nXXNvCPuUsxNxnj7H23heFM";
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(url, key);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase
